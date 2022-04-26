db2576f4a631cfdaae4b812e02a1c70c4e7cc1a6
pablomendezroyo <mendez4a@gmail.com>
2022-04-26 11:33:39 +0200

    implement supervisor and load keys on the fly
    remove unnecesary token header
    
    remove unnecesary migration
    
    fix graffiti name
    
    remove supervisor and use of certs
    
    edit cron to teku
    
    use of self signed certificate
    
    set certs
    
    generate cert dynamically
    
    fix logging

--- validator/entrypoint.sh
+++ validator/entrypoint.sh
@@ -1,98 +1,59 @@
 #!/bin/bash
 
-ERROR="[ ERROR ]"
-WARN="[ WARN ]"
-INFO="[ INFO ]"
+export CLIENT="teku"
+export NETWORK="prater"
+export VALIDATOR_PORT=3500
+export WEB3SIGNER_API="http://web3signer.web3signer-${NETWORK}.dappnode:9000"
+export CLIENT_API="http://validator.${CLIENT}-${NETWORK}.dappnode:${VALIDATOR_PORT}"
 
-CLIENT="teku"
-NETWORK="prater"
+if [[ $LOG_TYPE == "DEBUG" ]]; then
+  export LOG_LEVEL=0
+elif [[ $LOG_TYPE == "INFO" ]]; then
+  export LOG_LEVEL=1
+elif [[ $LOG_TYPE == "WARN" ]]; then
+  export LOG_LEVEL=2
+elif [[ $LOG_TYPE == "ERROR" ]]; then
+  export LOG_LEVEL=3
+else
+  export LOG_LEVEL=1
+fi
 
-function ensure_envs_exist() {
-    [ -z "${BEACON_NODE_ADDR}" ] && { echo "${ERROR} BEACON_NODE_ADDR is not set"; exit 1; }
-    [ -z "${HTTP_WEB3SIGNER}" ] && { echo "${ERROR} HTTP_WEB3SIGNER is not set"; exit 1; }
-    [ -z "${PUBLIC_KEYS_FILE}" ] && { echo "${ERROR} PUBLIC_KEYS_FILE is not set"; exit 1; }
-    [ -z "${SUPERVISOR_CONF}" ] && { echo "${ERROR} SUPERVISOR_CONF is not set"; exit 1; }
-    [ -z "$GRAFFITI" ] && echo "$ERROR: GRAFFITI is not set" && exit 1
-    [ ! -z "$GRAFFITI" ] && export EXTRA_OPTS="${EXTRA_OPTS} --validators-graffiti='${GRAFFITI}'" # Concatenate EXTRA_OPTS with existing var, otherwise supervisor will throw error
-}
+WEB3SIGNER_RESPONSE=$(curl -s -w "%{http_code}" -X GET -H "Content-Type: application/json" -H "Host: validator.${CLIENT}-${NETWORK}.dappnode" "${WEB3SIGNER_API}/eth/v1/keystores")
+HTTP_CODE=${WEB3SIGNER_RESPONSE: -3}
+CONTENT=$(echo "${WEB3SIGNER_RESPONSE}" | head -c-4)
+if [ "$HTTP_CODE" != "200" ]; then
+  echo "Failed to get keystores from web3signer, HTTP code: ${HTTP_CODE}, content: ${CONTENT}"
+else
+  PUBLIC_KEYS_WEB3SIGNER=($(echo "${CONTENT}" | jq -r 'try .data[].validating_pubkey'))
+  if [ ${#PUBLIC_KEYS_WEB3SIGNER[@]} -gt 0 ]; then
+    PUBLIC_KEYS_COMMA_SEPARATED=$(echo "${PUBLIC_KEYS_WEB3SIGNER[*]}" | tr ' ' ',')
+    echo "found validators in web3signer, starting vc with pubkeys: ${PUBLIC_KEYS_COMMA_SEPARATED}"
+    EXTRA_OPTS="--validators-external-signer-public-keys=${PUBLIC_KEYS_COMMA_SEPARATED} ${EXTRA_OPTS}"
+  fi
+fi
 
-# - Endpoint: http://web3signer.web3signer-prater.dappnode:9000/eth/v1/keystores
-# - Returns:
-# { "data": [{
-#     "validating_pubkey": "0x93247f2209abcacf57b75a51dafae777f9dd38bc7053d1af526f220a7489a6d3a2753e5f3e8b1cfe39b56f43611df74a",
-#     "derivation_path": "m/12381/3600/0/0/0",
-#     "readonly": true
-#     }]
-# }
-function get_public_keys() {
-    # Try for 3 minutes    
-    while true; do
-        if WEB3SIGNER_RESPONSE=$(curl -s -w "%{http_code}" -X GET -H "Content-Type: application/json" -H "Host: validator.${CLIENT}-${NETWORK}.dappnode" \
-        --retry 60 --retry-delay 3 --retry-connrefused "${HTTP_WEB3SIGNER}/eth/v1/keystores"); then
+# Loads envs into /etc/environment to be used by the cronjob
+env >>/etc/environment
+# start cron and disown it
+cron -f &
+disown
 
-            HTTP_CODE=${WEB3SIGNER_RESPONSE: -3}
-            CONTENT=$(echo ${WEB3SIGNER_RESPONSE} | head -c-4)
-
-            case ${HTTP_CODE} in
-                200)
-                    PUBLIC_KEYS_API=$(echo ${CONTENT} | jq -r 'try .data[].validating_pubkey')
-                    if [ -z "${PUBLIC_KEYS_API}" ]; then
-                        sed -i 's/autostart=true/autostart=false/g' $SUPERVISOR_CONF
-                        { echo "${WARN} no public keys found on web3signer"; break; }
-                    else 
-                        sed -i 's/autostart=false/autostart=true/g' $SUPERVISOR_CONF
-                        write_public_keys
-                        { echo "${INFO} found public keys: $PUBLIC_KEYS_API"; break; }
-                    fi
-                    ;;
-                403)
-                    if [[ "${CONTENT}" == *"Host not authorized"* ]]; then
-                        sed -i 's/autostart=true/autostart=false/g' $SUPERVISOR_CONF
-                        { echo "${WARN} client not authorized to access the web3signer api"; break; }
-                    fi
-                    break
-                    ;;
-                *)
-                    { echo "${ERROR} ${CONTENT} HTTP code ${HTTP_CODE} from ${HTTP_WEB3SIGNER}"; break; }
-                    ;;
-            esac
-            break
-        else
-            { echo "${WARN} web3signer not available"; continue; }
-        fi
-    done
-}
-
-# Ensure file will exists
-function clean_public_keys() {
-    rm -rf ${PUBLIC_KEYS_FILE}
-    touch ${PUBLIC_KEYS_FILE}
-}
-
-# Writes public keys
-# - by new line separated
-# - creates file if it does not exist
-function write_public_keys() {
-    echo "${INFO} writing public keys to file"
-    for PUBLIC_KEY in ${PUBLIC_KEYS_API}; do
-        if [ ! -z "${PUBLIC_KEY}" ]; then
-            echo "${INFO} adding public key: $PUBLIC_KEY"
-            echo "${PUBLIC_KEY}" >> ${PUBLIC_KEYS_FILE}
-        else
-            echo "${WARN} empty public key"
-        fi
-    done
-}
-
-########
-# MAIN #
-########
-
-ensure_envs_exist
-
-clean_public_keys
-
-get_public_keys
-
-# Execute supervisor with current environment!
-exec supervisord -c $SUPERVISOR_CONF
+exec /opt/teku/bin/teku --log-destination=CONSOLE \
+   validator-client \
+  --network=auto \
+  --data-base-path=/opt/teku/data \
+  --beacon-node-api-endpoint="$BEACON_NODE_ADDR" \
+  --validators-external-signer-url="$WEB3SIGNER_API" \
+  --metrics-enabled=true \
+  --metrics-interface 0.0.0.0 \
+  --metrics-port 8008 \
+  --metrics-host-allowlist=* \
+  --validator-api-enabled=true \
+  --validator-api-interface=0.0.0.0 \
+  --validator-api-port="$VALIDATOR_PORT" \
+  --validator-api-host-allowlist=* \
+  --validators-graffiti=\"${GRAFFITI}\" \
+  --validator-api-keystore-file=/usr/local/share/ca-certificates/server.crt \
+  --validator-api-keystore-password-file=/usr/local/share/ca-certificates/server.key \
+  --logging=ALL \
+  ${EXTRA_OPTS}
