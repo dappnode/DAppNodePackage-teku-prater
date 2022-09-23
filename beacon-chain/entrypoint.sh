2609ffe9a0bfdef94072b339e72596a86d05efd9
Voss <alex@visnovaventures.com>
2022-09-22 21:28:42 -0500

    add besu goerli

--- beacon-chain/entrypoint.sh
+++ beacon-chain/entrypoint.sh
@@ -10,6 +10,9 @@ case $_DAPPNODE_GLOBAL_EXECUTION_CLIENT_PRATER in
 "goerli-nethermind.dnp.dappnode.eth")
     HTTP_ENGINE="http://goerli-nethermind.dappnode:8551"
     ;;
+"besu-goerli.dnp.dappnode.eth")
+    HTTP_ENGINE="http://besu-goerli.dappnode:8551"
+    ;;
 "goerli-erigon.dnp.dappnode.eth")
     HTTP_ENGINE="http://goerli-erigon.dappnode:8551"
     ;;
