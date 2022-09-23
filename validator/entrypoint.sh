ac847e2912b3245ee663507da2c436fe76714665
Tropicar <zerozept@protonmail.com>
2022-09-23 12:02:17 +0200

    Fix typo and add logs

--- validator/entrypoint.sh
+++ validator/entrypoint.sh
@@ -18,13 +18,18 @@ fi
 
 
 
-if [[ "$EXIT_VALIDATOR" == "I want to exit my validators" ]];
-then
+if [[ "$EXIT_VALIDATOR" == "I want to exit my validators" ]]; then
+    echo "Check connectivity with the web3signer"
+    WEB3SIGNER_STATUS=$(curl -s  http://web3signer.web3signer-prater.dappnode:9000/healthcheck | jq '.status')
+    if [[ "$WEB3SIGNER_STATUS" == '"UP"' ]]; then
     echo "Proceeds to do the voluntary exit of the next keystores:"
     echo "$KEYSTORES_VOLUNTARY_EXIT"
-    exec/opt/teku/bin/teku voluntary-exit --beacon-node-api-endpoint=http://beacon-chain.teku-prater.dappnode:3500 \
+    echo yes | exec /opt/teku/bin/teku voluntary-exit --beacon-node-api-endpoint=http://beacon-chain.teku-prater.dappnode:3500 \
         --validators-external-signer-public-keys=$KEYSTORES_VOLUNTARY_EXIT \
-        --validators-external-signer-url=http://web3signer.web3signer-prater.dappnode:9000
+        --validators-external-signer-url=$WEB3SIGNER_API
+    else
+      echo "The web3signer-prater is not running or the teku package has not access to the web3signer"
+    fi
 fi
 
 
